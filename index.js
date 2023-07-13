7c86871097c9bf86ce7f6bd83137407d573d2ce2
Michael Cuppett <45700115+michaelcuppett@users.noreply.github.com>
2023-07-13 13:50:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', () => {
+  function subtractDate(date, daysToSubtract) {
+    let epochDate = date.setDate(date.getDate() - 2);
+    let result = new Date(0);
+    result.setUTCMilliseconds(epochDate);
+    return result;
+  }
+
+  let startDate = new Date(document.querySelector('.startdate').innerText);
+  let currentDate = new Date();
+  let currentDay = currentDate.getDate();
+
+
+  let dateOfUpdate = subtractDate(startDate, 2);
+  let prettyUpdate = dateOfUpdate.toLocaleDateString('en-us', { weekday: "long", month: "long", day: "numeric" });
+
+  let countdownHeading = '';
+  let countdownText = '';
+
+  countdownHeading = 'Check back soon';
+  countdownText = 'The bulletin and order of worship are typically available the Friday prior to a service. Does something look wrong? Send us an email at info@catonsvillepres.org to let us know.';
+
+  if (currentDate < subtractDate(startDate, 5)) {
+    countdownHeading = ('Check back on ').concat(prettyUpdate);
+    countdownText = 'The bulletin and order of worship are typically available the Friday prior to a service. Does something look wrong? Send us an email at info@catonsvillepres.org to let us know.';
+  }
+
+  if (currentDate >= subtractDate(startDate, 5)) {
+    countdownHeading = ('Check back this Friday');
+    countdownText = ('This page will be updated on or about ').concat(prettyUpdate, ' with the order of worship and bulletin.');
+  }
+
+  if (currentDate > dateOfUpdate) {
+    countdownHeading = 'Thanks for your patience.';
+    countdownText = ('We wanted to publish this page on ').concat(prettyUpdate, ', and we\'re sorry that it hasn\'t been updated on time. Send us an email at info@catonsvillepres.org and we\'ll do our best to get it updated ASAP.');
+  }
+
+  if (currentDate > startDate) {
+    countdownHeading = 'Request this bulletin';
+    countdownText = 'Even though the bulletin and order of worship wasn\'t published online for this service, we\'re happy to locate it for you. Send us an email at info@catonsvillepres.org to request a copy of this service bulletin.'
+  }
+
+  document.getElementById('mjc-cpc-countdown-heading').innerText = countdownHeading;
+  document.getElementById('mjc-cpc-countdown-text').innerText = countdownText;
+  document.querySelector('.mjc-cpc-countdown-block').classList.remove('mjc-cpc-countdown-hidden');
+})
